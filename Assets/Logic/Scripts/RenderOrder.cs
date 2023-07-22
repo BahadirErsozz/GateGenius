7a3d77690cc2d8ea7e7900623fd898d6255d402b
Alperen GÜLER <73526549+a-guler@users.noreply.github.com>
2023-07-22 23:13:39 +0300

    tablet arkaplani eklendi

---
+++ Assets/Logic/Scripts/RenderOrder.cs
@@ -0,0 +1,27 @@
+public static class RenderOrder
+{
+
+    public const float Background = 0;
+    public const float BackgroundOutline = Background + layerAbove;
+
+    public const float WireLow = BackgroundOutline + layerAbove;
+    public const float WireHigh = WireLow + layerAbove;
+    public const float BusWireLow = WireHigh + layerAbove;
+    public const float BusWireHigh = BusWireLow + layerAbove;
+    public const float WireEdit = BusWireHigh + layerAbove;
+
+    public const float PinNameDisplay = WireEdit + layerAbove;
+
+    public const float Chip = PinNameDisplay + layerAbove;
+    public const float ChipPin = Chip + layerAbove;
+
+    public const float EditablePin = ChipPin + layerAbove;
+    public const float EditablePinHigh = EditablePin + layerAbove;
+    public const float EditablePinPreview = EditablePinHigh + layerAbove;
+    public const float busConnectionDot = EditablePinPreview + layerAbove;
+
+    public const float ChipMoving = busConnectionDot + layerAbove;
+
+    // Step size
+    public const float layerAbove = -0.01f;
+}
