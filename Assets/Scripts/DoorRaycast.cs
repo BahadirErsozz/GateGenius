ffac253aa06ef5bfcf7fb781dfd2fdc1eb8ee892
BahadirErsozz <68385105+BahadirErsozz@users.noreply.github.com>
2023-08-21 00:56:08 +0300

    Finished Adding All Rooms

--- Assets/Scripts/DoorRaycast.cs
+++ Assets/Scripts/DoorRaycast.cs
@@ -6,11 +6,9 @@ using UnityEngine.UI;
 public class DoorRaycast : MonoBehaviour
 {
     [SerializeField] private int rayLength = 5;
-    
-    public DoorController rayCasterObj;
 
     private string interactableTag = "Door";
- 
+    private int doorCount = 1;
     void Update()
     {
         RaycastHit hit;
@@ -18,10 +16,8 @@ public class DoorRaycast : MonoBehaviour
         if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, rayLength)) {
             if(hit.collider.CompareTag(interactableTag)) {
                 if(Input.GetKeyDown(KeyCode.Mouse0)) {
-                    GameObject.Find("GameManager").GetComponent<GameManager>().TriggerWonStage(1);
-                }
-                else if(Input.GetKeyDown(KeyCode.Mouse1)){
-                    GameObject.Find("GameManager").GetComponent<GameManager>().TriggerWonStage(2);
+                    GameObject.Find("GameManager").GetComponent<GameManager>().TriggerWonStage(doorCount);
+                    doorCount++;
                 }
             }
         }
