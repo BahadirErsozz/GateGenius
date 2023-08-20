a54dec01ac05959e2f5805305b922f8ff9260702
Alperen GÜLER <73526549+a-guler@users.noreply.github.com>
2023-08-20 18:09:03 +0300

    IOPins

---
+++ Assets/IOPin.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : MonoBehaviour
+{
+    enum IOmode {INPUT,OUTPUT}
+    
+
+    [SerializeField] Color activeColor;
+    [SerializeField] Color inActiveColor;
+    [SerializeField] IOmode mode;
+    [SerializeField] Pin.HighlightState state;
+    [SerializeField] MeshRenderer display;
+
+    [SerializeField] Pin pin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mode == IOmode.INPUT)
+        {
+            pin.SetHighlightState(state);
+        }
+        else if (mode == IOmode.OUTPUT)
+        {
+            state = pin.activeHighlightState;
+        }
+        UpdateDisplayState();
+    }
+
+
+    public void UpdateDisplayState()
+    {
+        Pin.HighlightState activeHighlightState = state;
+        Color col = activeHighlightState switch
+        {
+            Pin.HighlightState.None => inActiveColor,
+            Pin.HighlightState.Highlighted => activeColor,
+            _ => Color.black
+        };
+
+        display.material.color = col;
+    }
+
+}
